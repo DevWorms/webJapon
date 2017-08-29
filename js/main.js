5fa3f90c296db0815a459b39ac4546a82c636892
Ricardo Suárez <ur.suarez@gmail.com>
2017-08-28 19:45:19 -0500

    mail

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+(function() {
+  $(document).ready(function() {
+    return $('#contact-form').submit(function(e) {
+      var email, message, name;
+      name = document.getElementById('name');
+      email = document.getElementById('email');
+      message = document.getElementById('description');
+      if (!name.value || !email.value || !message.value) {
+        alertify.error('Please check your entries');
+        return false;
+      } else {
+        $.ajax({
+          method: 'POST',
+          url: '//formspree.io/ur.suarez@gmail.com',
+          data: $('#contact-form').serialize(),
+          datatype: 'json'
+        });
+        e.preventDefault();
+        $(this).get(0).reset();
+        return alertify.success('Message sent');
+      }
+    });
+  });
+}).call(this);
+
 function openContent(evt, name) {
     var i, tabcontent, tablinks;
     
